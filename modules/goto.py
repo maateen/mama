01bc900a09627bbe5f9f65e011976dde16c5b159
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-07-17 01:12:35 +0600

    Applying PEP-8 convention

--- modules/goto.py
+++ modules/goto.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import sys,os, re
+import sys
+import os
+import re
 
 if len(sys.argv) >= 2:
     web = sys.argv[1]
-    web = web.replace(' ','+')
+    web = web.replace(' ', '+')
     web = web.lower()
-    
-    url = 'www.'+web
-    os.system('xdg-open '+url+' &')
+
+    url = 'www.' + web
+    os.system('xdg-open ' + url + ' &')
