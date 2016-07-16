a14bc42eb3d37f5f71d9917981119add4aa1ac85
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-07-16 15:13:26 +0600

    Aplying SSL

---
+++ modules/google.py
@@ -0,0 +1,12 @@
+import os, sys
+
+# Parsing arguments
+try:
+    if sys.argv[1]:
+        param = sys.argv[1]
+        execute = "xdg-open https://www.google.com/search?q=" + param + "&gws_rd=ssl"
+        os.system(execute)
+    else:
+        print("Not enough parameters to search on google.")
+except:
+    print("Not enough parameters to search on google.")
