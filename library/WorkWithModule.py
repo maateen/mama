4618b5cebedb3e0267d6a7ca5b7e0fcc76e6629a
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-07-17 01:38:11 +0600

    Fixing major bugs

--- library/WorkWithModule.py
+++ library/WorkWithModule.py
@@ -1,50 +1,40 @@
-from os.path import expanduser
-import os, gettext, unicodedata
+import os
 
-gettext.install('mama',os.path.dirname(os.path.abspath(__file__))+'/i18n/')
 
-# Permet de faire appel aux modules
-class workWithModule():
+class WorkWithModule():
+
     """
     @description: This class allows you to call external modules. If a call
     for an external module is detected by the parser then this class check
     the module's config file before extracting modules's parameter from the
     text you have pronounced
     """
-    def __init__(self,module_name,text,linker,plus,PID):
+
+    def __init__(self, module_name, text, linker, plus, PID):
         self.pid = PID
 
         try:
-            # on utilise un mot de liaison pour séparer l'appel du module
-            # des arguments à lui envoyer
-            # ex: Quelle est la météo à Paris
-            #     Quelle est la météo à Issy les moulineaux
-            #
-            # Le mot de liaison peut être " à "
-            sentence=text.lower()
-            # oblige to put this .encode('ASCII', 'ignore') for french
-            print(sentence)
-            sentence = unicodedata.normalize('NFKD', sentence)
-            print(sentence)
-            sentence=sentence.encode('ASCII', 'ignore')
-            print(sentence)
-            sentence=sentence.lower()
+            sentence = text.lower()
 
             if sentence.count(linker) > 0:
-                param =(sentence.split(linker,1)[1]).encode("utf-8")
+                param = (sentence.split(linker, 1)[1])
 
-                # on regarde si l'utilisateur veut transformer les ' ' en +
+                # we check whether the user wants to turn '' into +
                 if plus == '1':
-                    param=param.replace(' ','+')
-                print(param)
-                # commande qui sera exécutée
-                execute = expanduser('~')+'/.config/mama/modules/'+module_name+' '+'"'+param+'" &'
+                    param = param.replace(' ', '+')
+                # command that will be executed
+                parent_dir = os.path.dirname(os.path.abspath(__file__)).strip(
+                    'library')
+                module_dir = os.path.join(parent_dir, 'modules')
+                execute = 'python3 ' + module_dir + '/' + module_name + ' ' + param
+                print(execute)
                 os.system(execute)
             else:
-                message=_("you didn't say the linking word")
-                os.system('echo "'+message+'" > /tmp/mama/mama_error_'+self.pid)
+                message = "you didn't say the linking word"
+                os.system(
+                    'echo "' + message + '" > /tmp/mama/mama_error_' + self.pid)
 
         except IOError:
-            message = _('args file missing')
+            message = 'args file missing'
             os.system('echo "'+message+'" > /tmp/mama/mama_error_'+self.pid)
             sys.exit(1)
