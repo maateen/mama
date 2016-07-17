2bc6315f8ab3d23240523ba64374d4b998be0260
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-07-17 15:37:15 +0600

    Removing unwanted print and imports

--- library/Interface.py
+++ library/Interface.py
@@ -3,59 +3,50 @@ import json
 import os
 import sys
 import urllib.parse
-import xmltodict
 from os.path import expanduser
-from LocaleHelper import LocaleHelper
+
+import xmltodict
+from Listener import Listener
 from StringParser import StringParser
 
 
-# The interface class to start recording and communicating with Microsoft
-class interface():
+class Interface():
     """
-    @description: This class start the osd server, then start recording your voice before
+    @description: This class start the notifier, then start recording your voice before
     asking Microsoft for the speech-to-text. Then, the result is parsing in order to
     execute the associated action
     """
 
-    def __init__(self, client_id, api_key):
-        # make the program able to switch language
-        self.p = os.path.dirname(os.path.abspath(__file__)).strip('library')
-
-        LocaleHelper = LocaleHelper('en_EN')
-
-        self.lang = LocaleHelper.getLocale()
-        # this line can be remove if we modify the config/en_EN to config/en
-        # self.lang = self.lang+'_'+self.lang.upper()
+    def __init__(self, config):
+        self.p = os.path.dirname(os.path.abspath(__file__)).strip('librairy')
+        self.PID = str(os.getpid())
 
         # Initialization notifications
-        self.PID = str(os.getpid())
-        os.system('rm /tmp/mama/mama_*_' + self.PID + ' 2>/dev/null')
-        os.system('python3 ' + self.p + 'library/osd.py ' + self.PID + ' &')
-
-        # we play a sound to signal the start
-        os.system('play ' + self.p + 'resources/sound.wav &')
-        os.system('> /tmp/mama/mama_start_' + self.PID)
+        os.system(
+            'python3 ' + self.p + 'library/notifier.py ' + self.PID + ' &')
 
         # We launch the recorder
-        # Listener(self.PID)
-        self.sendto(client_id, api_key)
+        Listener(self.p, self.PID)
+        self.sendto(config)
 
-    def sendto(self, client_id, api_key):
+    def sendto(self, config):
         """
         @function: Send the wav file to Microsoft and start the parser
         """
 
         # configuration file
-        config = expanduser('~') + '/.config/mama/mama.xml'
-        default = self.p + 'config/' + self.lang + '/default.xml'
+        config_file = expanduser('~') + '/.config/mama/mama.xml'
+        default = self.p + 'config/' + 'en_EN' + '/default.xml'
 
-        if os.path.exists(config):
-            config_file = config
+        if os.path.exists(config_file):
+            config_file = config_file
         else:
             if os.path.exists(expanduser('~') + '/.config/mama') == False:
                 os.makedirs(expanduser('~') + '/.config/mama')
-            if os.path.exists(expanduser('~') + '/.config/mama/modules') == False:
-                os.system('cp -r ' + self.p + '/modules ' + expanduser('~') + '/.config/mama')
+            if os.path.exists(
+                            expanduser('~') + '/.config/mama/modules') == False:
+                os.system('cp -r ' + self.p + '/modules ' + expanduser(
+                    '~') + '/.config/mama')
             if os.path.exists(default) == False:
                 default = self.p + 'config/en_EN/default.xml'
 
@@ -63,35 +54,37 @@ class interface():
 
         print("config file:", config_file)
 
-        clientId = client_id
-        clientSecret = api_key
-        Host = "https://speech.platform.bing.com"
+        host = "https://speech.platform.bing.com"
 
         params = urllib.parse.urlencode(
-            {'grant_type': 'client_credentials', 'client_id': clientId, 'client_secret': clientSecret, 'scope': Host})
+            {'grant_type': 'client_credentials',
+             'client_id': config['client_id'],
+             'client_secret': config['api_key'], 'scope': host})
 
         headers = {"Content-type": "application/x-www-form-urlencoded"}
 
-        AccessTokenHost = "oxford-speech.cloudapp.net"
-        token_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "/token/issueToken")
+        access_token_host = "oxford-speech.cloudapp.net"
+        token_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                  "/token/issueToken")
 
         # Connecting to server to get the Microsoft Bing Speech API Access Token
-        conn = http.client.HTTPSConnection(AccessTokenHost, timeout=10)
+        conn = http.client.HTTPSConnection(access_token_host, timeout=10)
         conn.request("POST", token_path, params, headers)
         response = conn.getresponse()
+        print(response.status)
 
         if response.status == 200:
 
             data = response.read()
             conn.close()
-            accesstoken = data.decode("UTF-8")
+            access_token = data.decode("UTF-8")
 
             # Decoding the object from json
-            ddata = json.loads(accesstoken)
-            access_token = ddata['access_token']
+            decoded_data = json.loads(access_token)
+            access_token = decoded_data['access_token']
 
             # Reading the binary from wave file
-            f = open('output.wav', 'rb')
+            f = open('/tmp/mama/output.wav', 'rb')
             try:
                 body = f.read();
             finally:
@@ -101,29 +94,39 @@ class interface():
                        "Authorization": "Bearer " + access_token}
 
             # Connect to server to recognize the wave binary
-            conn = http.client.HTTPSConnection("speech.platform.bing.com", timeout=30)
+            conn = http.client.HTTPSConnection("speech.platform.bing.com",
+                                               timeout=30)
             conn.request("POST",
                          "/recognize/query?scenarios=ulm&appid=D4D52672-91D7-4C74-8AD8-42B1D98141A5&locale=en-US&device.os=wp7&version=3.0&format=xml&requestid=1d4b6030-9099-11e0-91e4-0800200c9a66&instanceid=1d4b6030-9099-11e0-91e4-0800200c9a66",
                          body, headers)
             response = conn.getresponse()
+            print(response.status)
 
             if response.status == 200:
 
                 data = response.read()
                 conn.close()
-                text = json.loads(json.dumps(xmltodict.parse(data.decode("UTF-8"))))
-                print(text['speechbox-root']['results']['result']['name'])
-                os.system('echo "' + text['speechbox-root']['results']['result'][
-                    'name'] + '" > /tmp/mama/mama_result_' + self.PID)
+                result = json.loads(
+                    json.dumps(xmltodict.parse(data.decode("UTF-8"))))
+                try:
+                    text = result['speechbox-root']['results']['result']['name']
+                except:
+                    text = ''
+                print(text)
+                os.system("echo '" + text + "' > /tmp/mama/mama_result_" +
+                          str(self.PID))
 
                 # parsing the results to find the action
-                sp = StringParser(text, config_file, self.PID)
+                StringParser(config, text,
+                             config_file, self.PID)
 
             else:
                 print(response.reason)
-                os.system('echo "' + response.reason + '" > /tmp/mama/mama_error_' + self.PID)
+                os.system(
+                    'echo "' + response.reason + '" > /tmp/mama/mama_error_' + self.PID)
                 sys.exit(1)
         else:
             print(response.reason)
-            os.system('echo "' + response.reason + '" > /tmp/mama/mama_error_' + self.PID)
+            os.system(
+                'echo "' + response.reason + '" > /tmp/mama/mama_error_' + self.PID)
             sys.exit(1)
