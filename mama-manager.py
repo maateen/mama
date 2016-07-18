f1fb9ccf559bad4db1d96f15cc193f5812736381
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-07-19 01:38:06 +0600

    Removing unused imports

--- mama-manager.py
+++ mama-manager.py
@@ -1,36 +1,24 @@
 import gi
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import os
 import sys
-import gettext
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/library')
-from MainWindow import *
-from localehelper import LocaleHelper
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/library')
+from MainWindow import MainWindow
 
-localeHelper = LocaleHelper()
-lang = localeHelper.getLocale()
 
-t=gettext.translation('mama',os.path.dirname(os.path.abspath(__file__))+'/i18n/',languages=[lang])
-t.install()
-
-#keep the old way for the moment
-#gettext.install('mama',os.path.dirname(os.path.abspath(__file__))+'/i18n/')
-
-# application principale
 class MyApplication(Gtk.Application):
+    """
+    @description: Initializing the main window for Mama Manager
+    """
     def __init__(self):
         Gtk.Application.__init__(self)
 
     def do_activate(self):
         win = MainWindow(self)
         win.show_all()
-        localeHelper = LocaleHelper()
-        lang = localeHelper.getLocale()
-
-        t=gettext.translation('mama',os.path.dirname(os.path.abspath(__file__))+'/i18n/',languages=[lang])
-        t.install()
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
