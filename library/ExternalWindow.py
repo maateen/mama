8a7c698b2c77cc03a2276102b0d14196df64fc81
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-07-19 18:02:59 +0600

    Decoded strings, PEP-8

--- library/ExternalWindow.py
+++ library/ExternalWindow.py
@@ -1,12 +1,14 @@
 import gi
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from os.path import expanduser
 import os
 import xml.etree.ElementTree as ET
 
-class externalWindow():
-    def __init__(self,store,iter=None):
+
+class ExternalWindow():
+    def __init__(self, store, iter=None):
         self.grid = Gtk.Grid()
         self.grid.set_border_width(5)
         self.grid.set_row_spacing(5)
@@ -14,11 +16,11 @@ class externalWindow():
         self.grid.set_hexpand(True)
         self.grid.set_column_spacing(2)
         self.grid.set_column_homogeneous(False)
-        label1 = Gtk.Label(_('key sentence'))
+        label1 = Gtk.Label('key sentence')
         label1.set_hexpand(True)
         label1.set_justify(Gtk.Justification.LEFT)
         label1.set_halign(Gtk.Align.START)
-        label2 = Gtk.Label(_('your command'))
+        label2 = Gtk.Label('your command')
         label2.set_justify(Gtk.Justification.LEFT)
         label2.set_halign(Gtk.Align.START)
         ll = Gtk.Label()
@@ -31,29 +33,30 @@ class externalWindow():
             self.entry2.set_text(store[iter][1])
 
         button = Gtk.Button.new_from_stock(Gtk.STOCK_OK)
-        button.connect("clicked",self.button_clicked,store,iter)
+        button.connect("clicked", self.button_clicked, store, iter)
         button_cancel = Gtk.Button.new_from_stock(Gtk.STOCK_CANCEL)
-        button_cancel.connect("clicked",self.do_destroy)
+        button_cancel.connect("clicked", self.do_destroy)
 
-        self.grid.attach(label1,0,0,11,1)
-        self.grid.attach(self.entry1,11,0,4,1)
-        self.grid.attach(label2,0,1,11,1)
-        self.grid.attach(self.entry2,11,1,4,1)
-        self.grid.attach(ll,0,2,15,1)
-        self.grid.attach(button_cancel,13,3,1,1)
-        self.grid.attach(button,14,3,1,1)
+        self.grid.attach(label1, 0, 0, 11, 1)
+        self.grid.attach(self.entry1, 11, 0, 4, 1)
+        self.grid.attach(label2, 0, 1, 11, 1)
+        self.grid.attach(self.entry2, 11, 1, 4, 1)
+        self.grid.attach(ll, 0, 2, 15, 1)
+        self.grid.attach(button_cancel, 13, 3, 1, 1)
+        self.grid.attach(button, 14, 3, 1, 1)
         self.grid.show_all()
 
-    def do_destroy(self,button):
+    def do_destroy(self, button):
         self.grid.destroy()
 
     def get_grid(self):
         return self.grid
 
-    def button_clicked(self,button,store,iter):
+    def button_clicked(self, button, store, iter):
         if iter is None:
             if self.entry1.get_text() is not '' and self.entry2.get_text() is not '':
-                store.append([self.entry1.get_text(),self.entry2.get_text(),_('external'), ' ',' '])
+                store.append([self.entry1.get_text(), self.entry2.get_text(),
+                              'external', ' ', ' '])
                 self.saveTree(store)
         elif iter is not None:
             store[iter][0] = str(self.entry1.get_text())
@@ -62,7 +65,7 @@ class externalWindow():
 
         self.grid.destroy()
 
-    def saveTree(self,store):
+    def saveTree(self, store):
         """
         @description: save the treeview in the mama.xml file
 
@@ -70,7 +73,7 @@ class externalWindow():
             the listStore attach to the treeview
         """
         # if there is still an entry in the model
-        config = expanduser('~') +'/.config/mama/mama.xml'
+        config = expanduser('~') + '/.config/mama/mama.xml'
         try:
             if not os.path.exists(os.path.dirname(config)):
                 os.makedirs(os.path.dirname(config))
@@ -82,21 +85,23 @@ class externalWindow():
                     if store[iter][0] != '' and store[iter][1] != '':
                         for s in store[iter][0].split('|'):
                             s = s.lower()
-                            s = s.replace('*',' ')
+                            s = s.replace('*', ' ')
                             Type = ET.SubElement(root, "entry")
-                            Type.set("name",unicode(store[iter][2],"utf-8"))
+                            Type.set("name", unicode(store[iter][2], "utf-8"))
                             Key = ET.SubElement(Type, "key")
-                            Key.text = unicode(s,"utf-8")
+                            Key.text = unicode(s, "utf-8")
                             Command = ET.SubElement(Type, "command")
-                            Command.text = unicode(store[iter][1],"utf-8")
+                            Command.text = unicode(store[iter][1], "utf-8")
                             Linker = ET.SubElement(Type, "linker")
                             Spacebyplus = ET.SubElement(Type, "spacebyplus")
-                            if store[iter][3] is not None and store[iter][4] is not None:
-                                Linker.text = unicode(store[iter][3],"utf-8")
-                                Spacebyplus.text = unicode(store[iter][4],"utf-8")
+                            if store[iter][3] is not None and store[iter][
+                                4] is not None:
+                                Linker.text = unicode(store[iter][3], "utf-8")
+                                Spacebyplus.text = unicode(store[iter][4],
+                                                           "utf-8")
 
-
-            tree = ET.ElementTree(root).write(config,encoding="utf-8",xml_declaration=True)
+            ET.ElementTree(root).write(config, encoding="utf-8",
+                                              xml_declaration=True)
 
         except IOError:
             print("Unable to write the file")
